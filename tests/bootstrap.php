91b62f1755bba63deadec0c0204a81df38ec30f5
Jan Petr <honzik.petr@gmail.com>
2016-05-10 11:31:06 +0200

    Travis tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -17,6 +17,10 @@ date_default_timezone_set('Europe/Prague');
 // Load framework
 require_once __DIR__.'/../MVC/loader.php';
 
+define('ALGOLIA_APP_ID', getenv('ALGOLIA_APPLICATION_ID'));
+define('ALGOLIA_SEARCH_ONLY_API_KEY', getenv('ALGOLIA_API_KEY'));
+define('ALGOLIA_ADMIN_API_KEY', getenv('ALGOLIA_API_KEY'));
+
 define('TEMP_DIR', __DIR__.'/tmp/'.lcg_value());
 @mkdir(TEMP_DIR, 0777, TRUE); // @ - base directory may already exist
 register_shutdown_function(function ()
